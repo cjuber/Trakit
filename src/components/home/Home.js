8150542e146bb8c4f1e85ab554afeb10c4b48a35
Chris Juber <cjuber5@gmail.com>
2021-01-24 17:03:25 -0700

    area card formatting

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,9 +15,18 @@ export default class Home extends Component {
                        <label> All<input type='radio' checked='checked'></input></label>
                     </div>
                    </div>
-                   <h1>Title</h1>
+                   <h1>List All</h1>
              
                <div className='areaBox'>
+                   <div className='title'>
+                        <h1 >Basement</h1> 
+                   </div>
+                   <div className='areas'>
+                        <h2>Cold Storage</h2><h2>(3)</h2>
+                   </div>
+                   <div className='items'>
+                        <h2>Items</h2><h2>(100)</h2>
+                   </div>
                </div>
             </div>
             </div>
